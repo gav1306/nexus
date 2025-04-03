65eb80c82fb6324d2aefa7dcda7d676165ca5443
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 03:11:48 +0530

    Update theme.jsx

--- src/providers/theme.jsx
+++ src/providers/theme.jsx
@@ -1,13 +1,7 @@
 "use client";
 
 import * as React from "react";
-const NextThemesProvider = dynamic(
-  () => import("next-themes").then((e) => e.ThemeProvider),
-  {
-    ssr: false,
-  }
-);
-import dynamic from "next/dynamic";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({ children, ...props }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
