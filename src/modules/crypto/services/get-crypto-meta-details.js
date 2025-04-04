825002471adc5a60f688dec3eaedeb172913c40b
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 16:02:32 +0530

    Create get-crypto-meta-details.js

---
+++ src/modules/crypto/services/get-crypto-meta-details.js
@@ -0,0 +1,26 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { coingeckoApi } from "@/services";
+import { cryptoQueryKeys } from "./query-keys";
+import { COIN_GECKO_NAMES } from "../utils";
+
+export const getCryptoMetaDetails = async (symbol) => {
+  const response = await coingeckoApi.get(
+    `/coins/${COIN_GECKO_NAMES[symbol]}`,
+    {
+      params: {
+        localization: false,
+        tickers: false,
+        market_data: true,
+      },
+    }
+  );
+
+  return response.data;
+};
+
+export const useSuspenseGetCryptoMetaDetails = (symbol) => {
+  return useSuspenseQuery({
+    queryKey: cryptoQueryKeys.metaDetails(symbol),
+    queryFn: () => getCryptoMetaDetails(symbol),
+  });
+};
