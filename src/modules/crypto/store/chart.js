ca0b023f464ecc7aec365cd4e60a08bc8ce974f5
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 06:59:00 +0530

    chart details filter values updated

--- src/modules/crypto/store/chart.js
+++ src/modules/crypto/store/chart.js
@@ -1,68 +1,64 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 export const initialFilterState = {
   interval: "15m",
   startTime: Date.now() - 24 * 60 * 60 * 1000,
   endTime: Date.now(),
-  limit: 1000,
 };
 
-export const useCryptoChartStore = create(
-    (set) => ({
-      filter: initialFilterState,
+export const useCryptoChartStore = create((set) => ({
+  filter: initialFilterState,
 
-      setFilter: (interval) =>
-        set((state) => {
-          switch (interval) {
-            case "15m": {
-              return {
-                filter: initialFilterState,
-              };
-            }
-            case "1h": {
-              return {
-                filter: {
-                  ...state.filter,
-                  interval: "1h",
-                  startTime: Date.now() - 7 * 24 * 60 * 60 * 1000,
-                },
-              };
-            }
-            case "6h": {
-              return {
-                filter: {
-                  ...state.filter,
-                  interval: "6h",
-                  startTime: Date.now() - 30 * 24 * 60 * 60 * 1000,
-                },
-              };
-            }
-            case "1d": {
-              return {
-                filter: {
-                  ...state.filter,
-                  interval: "1d",
-                  startTime: Date.now() - 90 * 24 * 60 * 60 * 1000,
-                },
-              };
-            }
-            case "1w": {
-              return {
-                filter: {
-                  ...state.filter,
-                  interval: "1w",
-                  startTime: Date.now() - 365 * 24 * 60 * 60 * 1000,
-                },
-              };
-            }
+  setFilter: (value) =>
+    set((state) => {
+      switch (value) {
+        case "1d": {
+          return {
+            filter: initialFilterState,
+          };
+        }
+        case "7d": {
+          return {
+            filter: {
+              ...state.filter,
+              interval: "1h",
+              startTime: Date.now() - 7 * 24 * 60 * 60 * 1000,
+            },
+          };
+        }
+        case "1m": {
+          return {
+            filter: {
+              ...state.filter,
+              interval: "4h",
+              startTime: Date.now() - 30 * 24 * 60 * 60 * 1000,
+            },
+          };
+        }
+        case "3m": {
+          return {
+            filter: {
+              ...state.filter,
+              interval: "12h",
+              startTime: Date.now() - 90 * 24 * 60 * 60 * 1000,
+            },
+          };
+        }
+        case "1y": {
+          return {
+            filter: {
+              ...state.filter,
+              interval: "3d",
+              startTime: Date.now() - 365 * 24 * 60 * 60 * 1000,
+            },
+          };
+        }
 
-            default: {
-              return {
-                filter: initialFilterState,
-              };
-            }
-          }
-        }),
-    })
-);
+        default: {
+          return {
+            filter: initialFilterState,
+          };
+        }
+      }
+    }),
+}));
