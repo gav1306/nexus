c4b18320f246f47176dd7ef317c4359e1c7c7710
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 16:03:24 +0530

    Update index.js

--- src/modules/crypto/services/index.js
+++ src/modules/crypto/services/index.js
@@ -2,3 +2,5 @@ export * from "./get-crypto-list";
 export * from "./query-keys";
 export * from "./get-favorite-crypto-list";
 export * from "./get-crypto-details";
+export * from "./get-crypto";
+export * from "./get-crypto-meta-details";
