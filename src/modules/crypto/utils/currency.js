dc6fd9104e6244eb7f8da7eff2e6f4b3aba6b59c
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 16:03:58 +0530

    Update currency.js

--- src/modules/crypto/utils/currency.js
+++ src/modules/crypto/utils/currency.js
@@ -2,6 +2,9 @@ export const formatCurrency = (value, options = {}) => {
   const currencyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
+    compactDisplay: "short",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
     ...options,
   });
   return currencyFormatter.format(value);
