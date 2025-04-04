42efd5f580560a80acc6198abf62af3aabe1598f
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 16:01:49 +0530

    Update hydrated-crypto-details.jsx

--- src/modules/crypto/hydrate/hydrated-crypto-details.jsx
+++ src/modules/crypto/hydrate/hydrated-crypto-details.jsx
@@ -1,14 +1,18 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient } from "@/services";
-import { cryptoQueryKeys, getCryptoDetails } from "../services";
+import { cryptoQueryKeys, getCrypto, getCryptoDetails } from "../services";
 import { CryptoChart } from "../components";
 import { initialFilterState } from "../store";
 
 export async function HydratedCryptoDetails({ symbol }) {
   const queryClient = getQueryClient();
   queryClient.prefetchQuery({
-    queryKey: cryptoQueryKeys.details({ ...initialFilterState, symbol }),
+    queryKey: cryptoQueryKeys.chartDetails({ ...initialFilterState, symbol }),
     queryFn: () => getCryptoDetails({ ...initialFilterState, symbol }),
+  });
+  queryClient.prefetchQuery({
+    queryKey: cryptoQueryKeys.details(symbol),
+    queryFn: () => getCrypto(symbol),
   });
   const dehydratedState = dehydrate(queryClient);
 
