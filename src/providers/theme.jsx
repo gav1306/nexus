b9b31416b2344c5cd751f7dd016a7d386c4b6df8
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-04-04 07:01:45 +0530

    theme hydration resolved

--- src/providers/theme.jsx
+++ src/providers/theme.jsx
@@ -1,8 +1,18 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
