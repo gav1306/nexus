2e5b2476714c99863a8765c5c4a86d92e0a3ee52
gayatri <gavk30@gmail.com>
2025-04-05 19:42:04 +0530

    fix: adjust z-index for background components to ensure proper layering

--- src/components/ui/background-beams.jsx
+++ src/components/ui/background-beams.jsx
@@ -59,7 +59,7 @@ export const BackgroundBeams = React.memo(({ className }) => {
   return (
     <div
       className={cn(
-        "absolute inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px]",
+        "absolute inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px] z-[-1]",
         className
       )}
     >
